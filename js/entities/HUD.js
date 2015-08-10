d7de75f2429617e52d89e7ef9e283f02e86214a7
Paul Joannon <hello@pauljoannon.com>
2015-08-10 23:44:57 +0200

    remove HUD's ScoreItem

--- js/entities/HUD.js
+++ js/entities/HUD.js
@@ -22,48 +22,5 @@ game.HUD.Container = me.Container.extend({
 
         // give a name
         this.name = "HUD";
-
-        // add our child score object at the top left corner
-        this.addChild(new game.HUD.ScoreItem(5, 5));
     }
-});
-
-
-/**
- * a basic HUD item to display score
- */
-game.HUD.ScoreItem = me.Renderable.extend({
-    /**
-     * constructor
-     */
-    init: function(x, y) {
-
-        // call the parent constructor
-        // (size does not matter here)
-        this._super(me.Renderable, 'init', [x, y, 10, 10]);
-
-        // local copy of the global score
-        this.score = -1;
-    },
-
-    /**
-     * update function
-     */
-    update : function () {
-        // we don't do anything fancy here, so just
-        // return true if the score has been updated
-        if (this.score !== game.data.score) {
-            this.score = game.data.score;
-            return true;
-        }
-        return false;
-    },
-
-    /**
-     * draw the score
-     */
-    draw : function (context) {
-        // draw it baby !
-    }
-
 });
